251a037394ae6bc2f8669392531b9f38a016c99d
Gaubee <GaubeeBangeel@Gmail.com>
2022-03-14 22:17:38 +0800

    :recycle: 改进yarn install相关的日志输出

--- packages/bfsp/bin/init.core.ts
+++ packages/bfsp/bin/init.core.ts
@@ -1,17 +1,32 @@
 import cp from "node:child_process";
 import { getYarnPath } from "../src";
 
-export const doInit = async (options: { root: string }, logger: PKGM.ConsoleLogger = console) => {
+export const doInit = async (options: { root: string }, consoleLogger: PKGM.ConsoleLogger = console) => {
   const { root } = options;
 
-  logger.info("linking dependencies");
+  consoleLogger.info("linking dependencies");
   const yarnPath = await getYarnPath();
 
   return new Promise((resolve) => {
     const proc = cp.spawn("node", [yarnPath], { cwd: root });
-    if (logger.isSuperLogger) {
-      proc.stdout && logger.warn.pipeFrom!(proc.stdout);
-      proc.stderr && logger.error.pipeFrom!(proc.stderr);
+    if (consoleLogger.isSuperLogger) {
+      const logger = consoleLogger as PKGM.Logger;
+      proc.stdout?.on("data", (chunk) => {
+        const log = String(chunk).trim();
+        if (log.startsWith("success")) {
+          logger.success.line(log);
+        } else {
+          logger.info.line(log);
+        }
+      });
+      proc.stderr?.on("data", (chunk) => {
+        const log = String(chunk).trim();
+        if (log.startsWith("warning")) {
+          logger.warn.line(log);
+        } else {
+          logger.error.line(log);
+        }
+      });
     } else {
       proc.stdout?.pipe(process.stdout);
       proc.stderr?.pipe(process.stderr);
--- packages/bfsp/bin/yarn/runner.ts
+++ packages/bfsp/bin/yarn/runner.ts
@@ -1,22 +1,34 @@
 import cp from "node:child_process";
+import { getYarnPath } from "../util";
+import {} from "@bfchain/util-extends-promise-out";
 export interface RunYarnOption {
   root: string;
   onExit?: () => void;
   onMessage?: (s: string) => void;
 }
 export const runYarn = (opts: RunYarnOption) => {
-  const proc = cp.exec(`corepack yarn`, { cwd: opts.root });
+  let proc: cp.ChildProcessWithoutNullStreams | undefined;
+  let killed = false;
   const ret = {
     stop() {
-      proc.kill();
+      killed = true;
+      proc?.kill();
     },
   };
 
-  proc.stdout?.on("data", (data: string) => opts.onMessage?.(data));
-  proc.stderr?.on("data", (data: string) => opts.onMessage?.(data));
-  proc.on("exit", (e) => {
-    opts.onExit?.();
-  });
+  (async () => {
+    const yarnPath = await getYarnPath();
+    if (killed) {
+      return;
+    }
+    proc = cp.spawn("node", [yarnPath, "install", "--non-interactive"], { cwd: opts.root }); // yarn install --non-interactive
+
+    proc.stdout?.on("data", (data) => opts.onMessage?.(String(data)));
+    proc.stderr?.on("data", (data) => opts.onMessage?.(String(data)));
+    proc.on("exit", (e) => {
+      opts.onExit?.();
+    });
+  })();
 
   return ret;
 };
