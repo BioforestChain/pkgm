cf4b5ad1206f3659f5ee21285ec959baac304c16
Ryan <smbserv@qq.com>
2022-03-25 18:28:52 +0800

    🔨 dev.mjs现在也执行dev:script了

--- dev.mjs
+++ dev.mjs
@@ -1,24 +1,44 @@
 import { spawn } from "node:child_process";
-let isBaseSuccess = false;
-function exec(cwd) {
-  const theCp = spawn("yarn", ["dev"], { cwd, shell: true });
+function exec(cwd, script) {
+  const theCp = spawn("yarn", [script], { cwd, shell: true });
   theCp.stdout.pipe(process.stdout);
   theCp.stderr.pipe(process.stderr);
   return theCp;
 }
-async function run() {
-  // 先执行base的编译
-  const theCp = exec("./packages/base");
-  for await (const data of theCp.stdout) {
-    const str = String(data);
-    if (!isBaseSuccess) {
-      if (/built in \d+ms/.test(str)) {
-        isBaseSuccess = true;
-        // 成功之后再执行bfsp和bfsw的编译
-        exec("./packages/bfsp");
-        exec("./packages/bfsw");
+
+async function* GenerateDevTask(packages) {
+  for (const p of packages) {
+    const taskName = `${p.dir} : ${p.name}`;
+    const cp = exec(p.dir, p.name);
+    if (p.wait) {
+      let isSuccess = false;
+      for await (const data of cp.stdout) {
+        const str = String(data);
+        if (!isSuccess) {
+          if (/built in \d+ms/.test(str)) {
+            yield taskName;
+            break;
+          }
+        }
       }
+    } else {
+      yield taskName;
     }
+  }
+}
+
+async function run() {
+  const packages = ["base", "bfsp", "bfsw"]
+    .map((x) => {
+      return ["dev", "dev:script"].map((s) => {
+        const wait = x === "base"; // base的任务要先编译
+        return { dir: `./packages/${x}`, name: s, wait };
+      });
+    })
+    .flat();
+
+  for await (const x of GenerateDevTask(packages)) {
+    console.log(x);
   }
 }
 run();
